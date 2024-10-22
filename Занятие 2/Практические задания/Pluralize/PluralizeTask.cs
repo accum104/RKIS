57f9bd32e3c4f8de894172d03895041d722327c7
GOL <164672521+accum104@users.noreply.github.com>
2024-10-22 13:25:44 +0300

    lesson 2 make pluralize

--- Занятие 2/Практические задания/Pluralize/PluralizeTask.cs
+++ Занятие 2/Практические задания/Pluralize/PluralizeTask.cs
@@ -4,7 +4,15 @@ public static class PluralizeTask
 {
 	public static string PluralizeRubles(int count)
 	{
-		// Напишите функцию склонения слова "рублей" в зависимости от предшествующего числительного count.
-		return "руб.";
+		 if (count % 100 >= 11 && count % 100 <= 14)
+                return "рублей";
+
+            int lastDigit = count % 10;
+            if (lastDigit == 1)
+                return "рубль";
+            else if (lastDigit >= 2 && lastDigit <= 4)
+                return "рубля";
+            else:
+		return "рублей";
 	}
 }
